65276bc3de0bb37ba88e4b4b787f6a0002ced443
Karen V <karenvo@usc.edu>
2019-09-18 01:56:48 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,40 +1,40 @@
 # define show class
 class Show < ActiveRecord::Base
 # inherits from ActiveRecord::Base
-  # def highest_rating
-  #   # return the highest value in the ratings column
-  #   self.maximum(:rating)
-  # end
-  #
-  # def most_popular_show
-  #   # return the show with the highest rating
-  #   self.where("name = maximum[:rating]")
-  # end
-  #
-  # def lowest_rating
-  #   #returns the lowest value in the ratings column
-  #   self.minimum(:rating)
-  # end
-  #
-  # def least_popular_show
-  #   # returns the show with the lowest rating
-  #   self.where("name = minimum[:rating]")
-  # end
-  #
-  # def ratings_sum
-  #   #returns the sum of all ratings_sum
-  #   self.sum(:rating)
-  # end
-  #
-  # def popular_shows
-  #   #returns an array of all the shows that have a rating greater than 5
-  #   # use where Active Record method
-  #   self.where("rating > ?", 5)
-  # end
-  #
-  # def shows_by_alphabetical_order
-  #   # returns an array of all the shows sorted by alphabetical order according o names
-  #   # hint: use order active record method
-  # end
+  def highest_rating
+    # return the highest value in the ratings column
+    self.maximum(:rating)
+  end
+
+  def most_popular_show
+    # return the show with the highest rating
+    self.where("name = maximum[:rating]")
+  end
+
+  def lowest_rating
+    #returns the lowest value in the ratings column
+    self.minimum(:rating)
+  end
+
+  def least_popular_show
+    # returns the show with the lowest rating
+    self.where("name = minimum[:rating]")
+  end
+
+  def ratings_sum
+    #returns the sum of all ratings_sum
+    self.sum(:rating)
+  end
+
+  def popular_shows
+    #returns an array of all the shows that have a rating greater than 5
+    # use where Active Record method
+    self.where("rating > ?", 5)
+  end
+
+  def shows_by_alphabetical_order
+    # returns an array of all the shows sorted by alphabetical order according o names
+    # hint: use order active record method
+  end
 
 end
