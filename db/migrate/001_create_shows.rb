e0b6250a263c2fb11b33979219a98fb613b6f29f
Karen V <karenvo@usc.edu>
2019-09-18 01:25:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,7 @@
 # datatype : string, rating : integer
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
-    create_table :show do |t|
+    create_table :shows do |t|
       t.string :name
       t.string :network
       t.string :day
