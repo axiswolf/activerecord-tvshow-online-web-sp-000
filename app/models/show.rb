188aa90170a3d6b8040d3d5dd71cb762f2c0bb2c
Karen V <karenvo@usc.edu>
2019-09-18 01:34:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,34 @@
 # define show class
 class Show < ActiveRecord::Base
 # inherits from ActiveRecord::Base
+  def highest_rating
+    # return the highest value in the ratings column
+  end
 
+  def most_popular_show
+    # return the show with the highest rating
+  end
+
+  def lowest_rating
+    #returns the lowest value in the ratings column
+  end
+
+  def least_popular_show
+    # returns the show with the lowest rating
+  end
+
+  def ratings_sum
+    #returns the sum of all ratings_sum
+  end
+
+  def popular_shows
+    #returns an array of all the shows that have a rating greater than 5
+    # use where Active Record method
+  end
+
+  def shows_by_alphabetical_order
+    # returns an array of all the shows sorted by alphabetical order according o names
+    # hint: use order active record method
+  end
+  
 end
