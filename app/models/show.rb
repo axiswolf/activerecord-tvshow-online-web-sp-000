d5b7b7e923d9070d804b07807161efcae360ce58
Karen V <karenvo@usc.edu>
2019-09-18 01:49:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,28 +8,28 @@ class Show < ActiveRecord::Base
 
   def most_popular_show
     # return the show with the highest rating
-    Show.where("name = maximum[:rating]")
+    self.where("name = maximum[:rating]")
   end
 
   def lowest_rating
     #returns the lowest value in the ratings column
-    Show.minimum(:rating)
+    self.minimum(:rating)
   end
 
   def least_popular_show
     # returns the show with the lowest rating
-    Show.where("name = minimum[:rating]")
+    self.where("name = minimum[:rating]")
   end
 
   def ratings_sum
     #returns the sum of all ratings_sum
-    Show.sum(:rating)
+    self.sum(:rating)
   end
 
   def popular_shows
     #returns an array of all the shows that have a rating greater than 5
     # use where Active Record method
-    Show.where("rating > ?", 5)
+    self.where("rating > ?", 5)
   end
 
   def shows_by_alphabetical_order
