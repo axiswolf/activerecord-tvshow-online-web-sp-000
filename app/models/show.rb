d60e15913a14107adb2cc477792f9a3071bc731e
Karen V <karenvo@usc.edu>
2019-09-18 02:05:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     # return the show with the highest rating
-    Show.where(rating: highest_rating)
+    Show.where(rating: highest_rating).first
   end
 
   def self.lowest_rating
