63b891ee2fb134e70492523781e940222cb8d100
Karen V <karenvo@usc.edu>
2019-09-18 01:11:21 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,7 @@
 # Write a migration code to create a shows table
 # table should have name, network, day, and ratings column_name
 # datatype : string, rating : integer
+class CreateShows < ActiveRecord::Migration[5.2]
+
+
+end
